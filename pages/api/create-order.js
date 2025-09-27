dcab938e7e29915c1270f2af3cab8c8930e38c1b
Harish <harishjoshi02112004@gmail.com>
2025-09-27 13:03:07 +0530

    adding

--- pages/api/create-order.js
+++ pages/api/create-order.js
@@ -6,8 +6,8 @@
 
     try {
       const instance = new Razorpay({
-        key_id: process.env.RAZORPAY_KEY_ID,
-        key_secret: process.env.RAZORPAY_KEY_SECRET
+        key_id: process.env.rzp_test_RMXCdZKpTrK5my,
+        key_secret: process.env.KeZMGS5iHGRRuS5wnppxGHhU
       });
 
       const options = {
